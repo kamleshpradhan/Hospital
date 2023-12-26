c775fe2e2d601573092b06f5fd9f71bf2042013e
Kamlesh <kamlesh.p@hng.co.in>
2023-12-26 11:08:52 +0530

    First Commit

---
+++ src/pages/products/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+export default function Product(){
+    return(
+        <div>
+            <p>Products Page</p>
+            <Link href="/home"> Home </Link>
+        </div>
+    )
+}
